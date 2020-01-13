e859e5ccfd6df52409aea5ce2879400d45a41db1
Welrikyson Felix <welrikyson.dev@outlook.com>
2020-01-13 17:34:06 -0400

    Reorderable List songs

--- lib/src/screens/worships_list/worship_list_preview.dart
+++ lib/src/screens/worships_list/worship_list_preview.dart
@@ -6,59 +6,56 @@ class WorshipListPreview extends StatefulWidget {
 }
 
 class _WorshipListPreviewState extends State<WorshipListPreview> {
+  List<Item> _items = MockItens().getAll();
   @override
   Widget build(BuildContext context) {
-    return Column(      
-      children: <Widget>[
-         SizedBox(
-          height: 20,
-        ),
-
-        Text('CELEBRAÇÂO'),
-        Text('Como é precioso'),
-        SizedBox(
-          height: 20,
-        ),
-
-        Text('CELEBRAÇÂO'),
-        Text('Como é precioso'),
-        SizedBox(
-          height: 20,
-        ),
-        Text('OFERTA'),
-        Text('Oferta de Amor'),
-        SizedBox(
-          height: 20,
-        ),
-
-        Text('VISITANTE'),
-        Text('Como é precioso'),
-        SizedBox(
-          height: 20,
-        ),
-        
-        Text('ADORAÇÃO'),
-        Text('Enche-me'),
-        SizedBox(
-          height: 20,
-        ),
-
-        Text('ADORAÇÃO'),
-        Text('Poderoso Deus'),
-        SizedBox(
-          height: 20,
-        ),
-
-        Text('ADORAÇÃO'),
-        Text('Formoso és'),
-        SizedBox(
-          height: 20,
-        ),
-      ],
+    return ReorderableListView(      
+      header: Text('List of songs'),
+      children: [
+        for (var item in _items) 
+          ListTile(
+            key: ValueKey(Item),
+            title: Text(item.title),
+          )
+      ], onReorder: (int oldIndex, int newIndex) {
+        setState(() {
+          _updateMyItems(oldIndex,newIndex);
+        });
+      },       
+      
     );
+  }
+
+  void _updateMyItems(int oldIndex, int newIndex) {
+      if(newIndex > oldIndex){
+        newIndex -= 1;
+      }
+
+      final Item item = _items.removeAt(oldIndex);
+      _items.insert(newIndex, item);
+
   }
 }
 
+
+class Item {
+  final String title;
+
+  Item({this.title});
+
+  
+}
+
+class MockItens {
+  List<Item> getAll(){
+    return <Item>[
+      Item(title: 'Rei dos Reis'),
+      Item(title: 'Vem essa é a hora da adoração'),
+      Item(title: 'Ao Único'),
+      Item(title: 'Doce Nome'),
+    ];
+  }
+}
 //TODO: poder montar lista de forma variada, dar direcionamento durante a montagem exemplo: leitura biblica e o qual as palavras chave
 //Definir limite de musicas para a lista
 //Conseguir melhor gerenciar o ensaio
