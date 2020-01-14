4853f8452d2585e115056d4c063959f05bc3e558
Welrikyson Felix <welrikyson.dev@outlook.com>
2020-01-14 17:52:39 -0400

    init crud of songs

---
+++ lib/src/screens/song/list_songs.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListSongs extends StatefulWidget {
+  @override
+  _ListSongsState createState() => _ListSongsState();
+}
+
+class _ListSongsState extends State<ListSongs> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
