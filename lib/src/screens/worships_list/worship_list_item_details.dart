b7452c488bd71f45250107aa1dbffd0fbc19267c
Welrikyson Felix <welrikyson.dev@outlook.com>
2020-01-14 15:12:31 -0400

    search bar

--- lib/src/screens/worships_list/worship_list_item_details.dart
+++ lib/src/screens/worships_list/worship_list_item_details.dart
@@ -1,4 +1,7 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:hillel/src/screens/worships_list/search_item.dart';
 import 'package:hillel/src/screens/worships_list/worship_list_preview.dart';
 
 class WorshipListItemDetails extends StatefulWidget {
@@ -11,19 +14,41 @@ class WorshipListItemDetails extends StatefulWidget {
 
 class _WorshipListItemDetailsState extends State<WorshipListItemDetails> {
   //String dropdownStyleValue = 'Oferta';
+  Future<void> openSearcherSongs() async {
+    var result = await showSearch(
+      context: context,
+      delegate: ItemSearch(
+        items: UnmodifiableListView( MockItens().getAll()),
+      ),
+    );
+    if (result != null) {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Detalhes"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: Center(
         child: ListView(
           children: <Widget>[
+            SizedBox(
+              height: 10,
+            ),
             DropdownButtonFormField<String>(
               isDense: true,
               decoration: InputDecoration(
-                  labelText: 'moment', prefixIcon: Icon(Icons.category)),
+                border: OutlineInputBorder(),
+                labelText: 'moment',
+                prefixIcon: Icon(Icons.category),
+              ),
               value: widget.item.style,
               icon: Icon(Icons.arrow_drop_down),
               hint: Text('Tipo lavagem'),
@@ -46,10 +71,29 @@ class _WorshipListItemDetailsState extends State<WorshipListItemDetails> {
                 );
               }).toList(),
             ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              readOnly: true,
+              decoration: InputDecoration(
+                labelText: 'song',
+                border: OutlineInputBorder(),
+                prefixIcon: Icon(Icons.music_note),
+                hintText: 'song',
+                suffixIcon: Icon(Icons.library_music),
+              ),
+              onTap: openSearcherSongs,
+            ),
+            SizedBox(
+              height: 10,
+            ),
             DropdownButtonFormField<String>(
               isDense: true,
               decoration: InputDecoration(
-                  labelText: 'song', prefixIcon: Icon(Icons.music_note)),
+                  border: OutlineInputBorder(),
+                  labelText: 'song',
+                  prefixIcon: Icon(Icons.music_note)),
               value: widget.item.style,
               icon: Icon(Icons.arrow_drop_down),
               hint: Text('Tipo lavagem'),
---
+++ lib/src/widgets/search.dart
@@ -0,0 +1,77 @@
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+import 'package:hillel/src/screens/worships_list/worship_list_preview.dart';
+
+class ItemSearch extends SearchDelegate<Item> {
+  final UnmodifiableListView<Item> items;
+
+  ItemSearch(this.items);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    var results = items
+        .where((a) => a.title.toLowerCase().contains(query.toLowerCase()));
+
+    return ListView(
+      children: results
+          .map<ListTile>((a) => ListTile(
+                title: Text(a.title,
+                    style: Theme.of(context)
+                        .textTheme
+                        .subhead
+                        .copyWith(fontSize: 16.0)),
+                leading: Icon(Icons.book),
+                onTap: () async {
+                  // if (await canLaunch(a.url)) {
+                  //   await launch(a.url);
+                  // }
+                  close(context, a);
+                },
+              ))
+          .toList(),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final results = items
+        .where((a) => a.title.toLowerCase().contains(query.toLowerCase()));
+
+    return ListView(
+      children: results
+          .map<ListTile>((a) => ListTile(
+                title: Text(a.title,
+                    style: Theme.of(context).textTheme.subhead.copyWith(
+                          fontSize: 16.0,
+                          color: Colors.blue,
+                        )),
+                onTap: () {
+                  close(context, a);
+                },
+              ))
+          .toList(),
+    );
+  }
+}
